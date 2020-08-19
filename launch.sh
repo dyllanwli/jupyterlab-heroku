7b784af6e026bec4d94310fc87e7a6932ed3a2a8
dyllanwli <messsking@gmail.com>
2020-08-19 17:37:38 +0000

    add plugins

--- launch.sh
+++ launch.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# jupyterlab install plugins
 jupyter serverextension enable --py jupyterlab --sys-prefix
+jupyter labextension install @ryantam626/jupyterlab_code_formatter
+pip install jupyterlab_code_formatter
+
+# pip plugins
+pip install black isort
 
 mkdir -p root
 cd root
